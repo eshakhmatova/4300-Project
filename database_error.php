0362af1a2caabed2058cafee93d72c8ee0261e03
es01747 <es01747@uga.edu>
2022-03-23 13:26:59 -0400

    Added database connection

---
+++ database_error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>E-Commerce App</title>
+        <link rel="stylesheet" type="type/css" href="styles.css"/>
+    </head>
+    <body>
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error connecting to the database.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+    </main>
+    </body>
+</html>
